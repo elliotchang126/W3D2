f9c082f46ed9dd8bd0a006419cea61d8b1811323
harivmasoor <harimasoor@gmail.com>
2023-06-06 14:30:53 -0700

    test

--- Board.rb
+++ Board.rb
@@ -1,5 +1,28 @@
 class Board
+    def initialize(num)
+        @board = Array.new(num) {Array.new(num) "_"}
+        @size = num*num
+        @num = num
+    end
 
-    
+    def guess
+        guessed_pos =
+    end
+
+    def populate
+        alpha = ["A".."Z"]
+        short_alpha = [alpha[0]..alpha[num]]
+        @card_1 = []
+        @card_2 = []
+        @num.times do 
+            @card_1 << Card.new(short_alpha.sample)
+    end
+
+    def render 
+    end
+
+    def won?
+    end
 
 end
+
